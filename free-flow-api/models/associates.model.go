b93f8d0730ae70fbd4294b3f1d8eaa2c3844f1ec
Amon Victor <amon.victor.develops@gmail.com>
2025-08-16 13:12:42 +0300

    Added the Associates  model

--- free-flow-api/models/associates.model.go
+++ free-flow-api/models/associates.model.go
@@ -8,25 +8,24 @@ import (
 	"gorm.io/gorm"
 )
 
-type Entity struct {
-	ID        uuid.UUID      `json:"id" gorm:"type:uuid;primaryKey"`
+type Associate struct {
+	ID        uuid.UUID      `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
-
-	CompanyName string `json:"companyname" gorm:"size:100;not null"`
-	Contact     string `json:"contact"`
 
 	UserID uint   `json:"user_id" gorm:"not null"`
 	Name   string `json:"name" gorm:"not null"`
 	Email  string `json:"email"`
 	Phone  string `json:"phone"`
 
+	Skills string `json:"skills"` // "design,frontend,backend"
+
 	User User `json:"-" gorm:"foreignKey:UserID"`
-	// Projects []Project `json:"projects" gorm:"foreignKey:EntityID"`
+	// Projects []Project `json:"projects" gorm:"many2many:project_associates;"`
 }
 
-func (u *Entity) BeforeCreate(tx *gorm.DB) (err error) {
+func (u *Associate) BeforeCreate(tx *gorm.DB) (err error) {
 	if u.ID == uuid.Nil {
 		u.ID = uuid.New()
 	}
